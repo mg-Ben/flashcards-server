e8f7ea6bbcf30575a7158f8de0fa33c71f52e80c
mg-Ben <benjaminmartin1008@gmail.com>
2024-06-21 20:58:48 +0200

    [refactor]: Refactor MySQL model

--- src/models/mysql/Deck.ts
+++ src/models/mysql/Deck.ts
@@ -1,50 +1,36 @@
-import mysql, { type RowDataPacket } from 'mysql2/promise'
 import { type DeckModelType, type Deck } from '../../types/Deck'
-
-const DEFAULT_CONFIG = {
-  host: 'localhost',
-  user: 'root',
-  port: 3306,
-  password: 'root',
-  database: 'flashcards_db'
-}
-
-const connectionString =
-  process.env.DATABASE_URL ??
-  'mysql://' +
-    DEFAULT_CONFIG.user +
-    ':' +
-    DEFAULT_CONFIG.password +
-    '@' +
-    DEFAULT_CONFIG.host +
-    ':' +
-    DEFAULT_CONFIG.port +
-    '/' +
-    DEFAULT_CONFIG.database
-const connection = await mysql.createConnection(connectionString)
+import MySQLConnection from './MySQLConnection'
 
 export class DeckModel implements DeckModelType {
+  readonly #mysqlConnection: MySQLConnection
+
+  constructor () {
+    this.#mysqlConnection = new MySQLConnection()
+  }
+
+  // TODO get deck function
   async get (): Promise<string> {
     return 'OK'
   }
 
   async getAll (): Promise<Deck[]> {
-    const [queryResult] = await connection.query<RowDataPacket[]>(
-      'SELECT BIN_TO_UUID(uuid) AS uuid, name, description FROM decks;'
-    )
-    // Convert query result into Deck[] type
+    const query = 'SELECT BIN_TO_UUID(uuid) AS uuid, name, description FROM decks;'
+    const queryResult = await this.#mysqlConnection.runQuery(query)
+
     const result: Deck[] = []
     queryResult.forEach((row) => {
-      // Convert each row into Deck type
       const deckObject: Deck = {
         uuid: row.uuid,
         name: row.name,
         description: row.description
       }
-      // Validate data:
+
+      // TODO validate data
       // validateDataDeck(deckObject)
+
       result.push(deckObject)
     })
+
     return result
   }
 }
---
+++ src/models/mysql/MySQLConnection.ts
@@ -0,0 +1,56 @@
+import mysql, { type RowDataPacket } from 'mysql2/promise'
+
+interface ConnectionParams {
+  user: string
+  password: string
+  host: string
+  port: string
+  databaseName: string
+}
+
+export default class MySQLConnection {
+  readonly #connection: Promise<mysql.Connection> | undefined
+  readonly #uri: string
+
+  constructor () {
+    this.#uri = this.#getConnectionUriFromParams()
+    this.#connection = this.getConnection()
+  }
+
+  #getConnectionUriFromParams (): string {
+    const { user, password, host, port, databaseName } = this.#getConnectionParams()
+    return `mysql://${user}:${password}@${host}:${port}/${databaseName}`
+  }
+
+  #getConnectionParams (): ConnectionParams {
+    const user = process.env.MYSQL_USER ?? 'root'
+    const password = process.env.MYSQL_PASSWORD ?? 'root'
+    const host = process.env.MYSQL_HOST ?? 'localhost'
+    const port = process.env.MYSQL_PORT ?? '3306'
+    const databaseName = process.env.MYSQL_DATABASE_NAME ?? 'flashcards_db'
+
+    return {
+      user,
+      password,
+      host,
+      port,
+      databaseName
+    }
+  }
+
+  async getConnection (): Promise<mysql.Connection> {
+    if (this.#connection !== undefined) {
+      return await this.#connection
+    }
+
+    return await mysql.createConnection(this.#uri)
+  }
+
+  async runQuery (
+    query: string
+  ): Promise<RowDataPacket[]> {
+    const connection = await this.getConnection()
+    const [queryResult] = await connection.query<RowDataPacket[]>(query)
+    return queryResult
+  }
+}
