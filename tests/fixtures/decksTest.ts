d053fc843bc89341120633738556be0390ceab22
mg-Ben <benjaminmartin1008@gmail.com>
2024-06-22 19:51:08 +0200

    [test]: Add API GET /decks test for local model with mock

--- tests/fixtures/decksTest.ts
+++ tests/fixtures/decksTest.ts
@@ -1,16 +1,17 @@
-export const decksTest = [{
-  uuid: '89c09a27-2194-11ef-8aa6-6c240852178d',
-  name: 'Japanese vocabulary',
-  description: 'My deck with japanese vocabulary'
-},
-{
-  uuid: '89c2e38e-2194-11ef-8aa6-6c240852178d',
-  name: 'Bash commands',
-  description: 'My deck with Linux commands'
-},
-{
-  uuid: '89c3bcf8-2194-11ef-8aa6-6c240852178d',
-  name: 'Mysterious deck',
-  description: 'Now it is not a mysterious deck'
-}
+export const decksTest = [
+  {
+    uuid: '89c09a27-2194-11ef-8aa6-6c240852178d',
+    name: 'Japanese vocabulary',
+    description: 'My deck with japanese vocabulary'
+  },
+  {
+    uuid: '89c2e38e-2194-11ef-8aa6-6c240852178d',
+    name: 'Bash commands',
+    description: 'My deck with Linux commands'
+  },
+  {
+    uuid: '89c3bcf8-2194-11ef-8aa6-6c240852178d',
+    name: 'Mysterious deck',
+    description: 'Now it is not a mysterious deck'
+  }
 ]
--- tests/server-with-local.test.ts
+++ tests/server-with-local.test.ts
@@ -1,19 +1,24 @@
-import { afterAll, describe, expect, it } from '@jest/globals'
+import { afterAll, describe, expect, it, jest } from '@jest/globals'
 import request from 'supertest'
-import { app, server } from '../src/server-with-local'
+import { app, server } from '@/server-with-local'
+import { DeckModel } from '@/models/local/Deck'
+import { decksTest } from './fixtures/decksTest'
+
+jest
+  .spyOn(DeckModel.prototype, 'getAll')
+  .mockImplementation(async () => await Promise.resolve(decksTest))
 
 afterAll(() => {
   server.close()
 })
 
 describe('deck module', () => {
-  it('GET is working', async () => {
-    const RESPONSE = {
-      message: 'OK'
-    }
+  it('should Get /decks return all decks', async () => {
     const response = await request(app).get('/decks')
 
+    console.log('hello world')
+
     expect(response.statusCode).toBe(200)
-    expect(response.body).toStrictEqual(RESPONSE)
+    expect(response.body).toStrictEqual(decksTest)
   })
 })
